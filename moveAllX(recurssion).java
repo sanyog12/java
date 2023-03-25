f44c455764440c220a53065852ce8a85ce540d1e
sanyog12 <94354794+sanyog12@users.noreply.github.com>
2023-03-25 13:05:44 +0530

    Add files via upload

---
+++ moveAllX(recurssion).java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main{
+    public static void moveAllX(String str, int idx, int count, String newString){
+        
+        if(idx == str.length()){
+        
+            for(int i = 0; i < count; i++){
+        
+                newString += 'x';
+            }
+        
+            System.out.println(newString);
+        
+            return;
+        }
+        if(str.charAt(idx) == 'x'){
+        
+            count++;
+            moveAllX(str,idx+1,count,newString);
+            
+        }
+        else{
+            
+            newString += str.charAt(idx);
+            moveAllX(str,idx+1,count,newString);
+            
+        }
+    }
+	public static void main(String[] args) {
+	    
+	    String str = "sdfxhlxxxkjhkxkjxh";
+	    moveAllX(str, 0 , 0, "");
+	    
+	}
+}
