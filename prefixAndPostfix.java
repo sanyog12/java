a129ee4005dadda413093a529df7982c5bdff704
sanyog12 <94354794+sanyog12@users.noreply.github.com>
2023-04-23 10:37:18 +0530

    Add files via upload

---
+++ prefixAndPostfix.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args){
+        int a  = 10;
+        //postfix
+        System.out.println(a++);
+        System.out.println(a);
+        //prefix
+        System.out.println(a--);
+        System.out.println(a);
+
+    }
+}
