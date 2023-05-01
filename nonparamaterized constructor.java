7769f14901429225e84e0475722860d7dfcaf9d7
sanyog12 <94354794+sanyog12@users.noreply.github.com>
2023-05-01 16:47:16 +0530

    Add files via upload

---
+++ nonparamaterized constructor.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+    public class Main{
+        int a;
+        int c;
+        //intialization
+        Main(){
+            a = 20;
+            c = 39;
+        }
+
+    public static void main(String[] args){
+    Main g = new Main();
+    System.out.println(g.a);
+    System.out.println(g.c);
+    }
+}
