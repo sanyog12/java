c37754100c10d2e05b8448d54f2ac7c750f490e7
sanyog12 <94354794+sanyog12@users.noreply.github.com>
2023-05-05 11:06:11 +0530

    Add files via upload

---
+++ overloadMainMethod.java
@@ -0,0 +1,14 @@
+package laudaLassun;
+
+public class overloadMainMethod
+{
+    public static void main(){
+        System.out.println("we can overload main methord");
+    }
+
+
+    public static void main(String[]args){
+
+        main();
+    }
+}
