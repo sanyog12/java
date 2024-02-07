de5c6504154e58ea2d36ecf5f740667549c35f14
Sanyog Mane <94354794+sanyog12@users.noreply.github.com>
2024-02-07 22:44:54 +0530

    Add files via upload

---
+++ Best Time To Buy Stocktxt.java
@@ -0,0 +1,18 @@
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+        
+        return maxProfit;
+    }
+}
