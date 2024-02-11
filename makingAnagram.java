7ee61c2eb37583af269851a91230835af3ca8dff
Sanyog Mane <94354794+sanyog12@users.noreply.github.com>
2024-02-12 00:08:17 +0530

    Add files via upload

---
+++ makingAnagram.java
@@ -0,0 +1,27 @@
+https://www.hackerrank.com/challenges/making-anagrams/problem?isFullScreen=true
+
+  
+public static int makingAnagrams(String s1, String s2) {
+    // Write your code here
+    int []c = new int[26];
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+    
+    for(int i = 0; i < s1.length(); i++){
+        c[s1.charAt(i) - 'a']++;
+    }
+    for(int i = 0; i < s2.length(); i++){
+        c[s2.charAt(i) - 'a']--;
+    }
+    
+    int total = 0;
+    
+    for(int i:c){
+        total += Math.abs(i);
+    }
+    
+    return total;   
+
+    }
+
+}
